caaa41fe9c33858493f5e915d8bf59b24916e20c
Sandile Keswa <me@sandile.io>
2022-10-08 19:14:55 -0400

    Created a hello world app in XCode

---
+++ apple/Sonic/Sonic/SonicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonicApp.swift
+//  Sonic
+//
+//  Created by Sandile Keswa on 10/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SonicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
