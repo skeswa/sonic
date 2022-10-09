403d812cea03898999f870fd500750cb87ab210f
Sandile Keswa <me@sandile.io>
2022-10-08 20:51:47 -0400

    Looks like we can request health data now

--- apple/Sonic/Sonic/ContentView.swift
+++ apple/Sonic/Sonic/ContentView.swift
@@ -5,12 +5,24 @@
 //  Created by Sandile Keswa on 10/8/22.
 //
 
+import HealthKit
 import SwiftUI
 
 struct ContentView: View {
+  private let healthStore = HKHealthStore()
+  private let objectTypes: Set<HKObjectType> = [
+      HKObjectType.activitySummaryType()
+  ]
+  
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    VStack(alignment: .leading, spacing: 4.0) {
+      Button("Do the thing") {
+        healthStore.requestAuthorization(toShare: nil, read: objectTypes) { (success, error) in
+          print("OK looks good", success)
+            // Authorization request finished, hopefully the user allowed access!
+        }
+      }
+    }
   }
 }
 
