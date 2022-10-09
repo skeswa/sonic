86b93c454c2065179242082c592dcb02bee2149c
Sandile Keswa <me@sandile.io>
2022-10-08 21:16:04 -0400

    got some print messages

--- apple/Sonic/Sonic/ContentView.swift
+++ apple/Sonic/Sonic/ContentView.swift
@@ -11,15 +11,45 @@ import SwiftUI
 struct ContentView: View {
   private let healthStore = HKHealthStore()
   private let objectTypes: Set<HKObjectType> = [
-      HKObjectType.activitySummaryType()
+    HKObjectType.activitySummaryType()
   ]
   
   var body: some View {
     VStack(alignment: .leading, spacing: 4.0) {
       Button("Do the thing") {
         healthStore.requestAuthorization(toShare: nil, read: objectTypes) { (success, error) in
-          print("OK looks good", success)
-            // Authorization request finished, hopefully the user allowed access!
+          if success {
+            print("OK it works I think")
+            
+            let calendar = Calendar.autoupdatingCurrent
+            
+            var dateComponents = calendar.dateComponents(
+              [ .year, .month, .day ],
+              from: Date()
+            )
+            
+            // This line is required to make the whole thing work
+            dateComponents.calendar = calendar
+            
+            let predicate = HKQuery.predicateForActivitySummary(with: dateComponents)
+            
+            let query = HKActivitySummaryQuery(predicate: predicate) { (query, summaries, error) in
+              
+              guard let summaries = summaries, summaries.count > 0
+              else {
+                print("Hmmmm")
+                dump(summaries?.count)
+                dump(error)
+                // No data returned. Perhaps check for error
+                return
+              }
+              
+              // Handle the activity rings data here
+              print("Yeahboi")
+              dump(summaries)
+            }
+            healthStore.execute(query)
+          }
         }
       }
     }
